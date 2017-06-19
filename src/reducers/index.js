1fbb7314d2ff9e5d3adbc6ef5beabb31f9494d14
xyapple <ydeascentis@gmail.com>
2017-06-19 11:29:30 -0400

    Add the modalreducer into the rootreducer--index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,9 +4,12 @@
 
 import { combineReducers } from 'redux';
 import GifsReducer from './gifs';
+import ModalReducer from './modal';
+
 
 const rootReducer = combineReducers({
   gifs: GifsReducer
+  modal:ModalReducer
 });
 
 export default rootReducer;
